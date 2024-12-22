bf11f86903f27d88d89d44b1acb63a2d9038f02f
VSV <voloshin.dda@gmail.com>
2024-12-22 22:08:33 +0100

    Delete task-3.js

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputEl = document.querySelector('#name-input');
-const outputEl = document.querySelector('#name-output');
-
-inputEl.addEventListener('input', event => {
-  const value = event.target.value.trim();
-  outputEl.textContent = value === '' ? 'Anonymous' : value;
-});
